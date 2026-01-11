acdbbe6bc9f2841ff8af1ec438c72d4e3ada3b28
Ntare-GAMA <a.mugisha1@alustudent.com>
2026-01-11 15:11:01 +0200

    csharp

---
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# is an object-oriented programming language.";
+        str = str.Substring(49, 21) + str.Substring(114, 4) + str.Substring(142, 3);
+        Console.WriteLine(str);
+    }
+}
