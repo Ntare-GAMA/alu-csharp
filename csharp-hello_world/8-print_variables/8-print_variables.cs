4767dcce6971cde1447bf4045b15d203e07a423c
Ntare-GAMA <a.mugisha1@alustudent.com>
2026-01-12 11:10:24 +0200

    whatcha say

--- csharp-hello_world/8-print_variables/8-print_variables.cs
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -4,8 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        string str1 = "Mission";
+        string str2 = "Street";
         int number = 972;
-        string street = "Mission Street";
-        Console.WriteLine($"{number} {street}");
+        Console.WriteLine("{0} {1} {2}", number, str1, str2);
     }
 }
