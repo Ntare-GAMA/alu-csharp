d9ab2dc7d3d68cddb5650f1a59d70e2339bb598b
GAMA <a.mugisha1@alustudent.com>
2026-01-18 22:03:49 +0200

    Update 8-print_variables.cs

--- csharp-hello_world/8-print_variables/8-print_variables.cs
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -1,12 +1,12 @@
 using System;
+
 class Program
 {
     static void Main(string[] args)
     {
-        int number = 98;
-        string street = "Battery";
-        string type = "Street";
-        
-        Console.WriteLine($"{number} {street} {type}");
+        string str1 = "Street";
+        string str2 = "Mission";
+        int int1 = 972;
+        Console.WriteLine($"{int1} {str2} {str1}");
     }
 }
