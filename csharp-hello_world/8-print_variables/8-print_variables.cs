c0c4e092892e7a6927eda7961e5c5e9c3fb9bdc0
GAMA <a.mugisha1@alustudent.com>
2026-01-18 17:58:00 +0200

    Refactor variable names and update Console.WriteLine

--- csharp-hello_world/8-print_variables/8-print_variables.cs
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -1,12 +1,12 @@
-﻿using System;
-
+using System;
 class Program
 {
     static void Main(string[] args)
     {
-        string str1 = "Mission";
-        string str2 = "Street";
         int number = 972;
-        Console.WriteLine("{0} {1} {2}", number, str1, str2);
+        string street = "Mission";
+        string type = "Street";
+        
+        Console.WriteLine($"{number} {street} {type}");
     }
 }
