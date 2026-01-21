efe3061205bb55dcb7fcf82496fc6030aea9f56e
Ntare-GAMA <a.mugisha1@alustudent.com>
2026-01-21 11:32:26 +0200

    holl up

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = i;
+            Console.Write(i);
+            if (i < size - 1)
+                Console.Write(" ");
+        }
+        Console.WriteLine();
+        return arr;
+    }
+}
