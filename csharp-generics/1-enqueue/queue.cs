00bc3c59f5c9ea0a3e5ea20ef83d320c34bdfe81
GAMA <a.ntare@alustudent.com>
2026-06-16 18:22:43 +0300

    Add XML documentation comments to Queue class

--- csharp-generics/1-enqueue/queue.cs
+++ csharp-generics/1-enqueue/queue.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
+/// <summary>
+/// Represents a generic first-in-first-out (FIFO) collection.
+/// </summary>
+/// <typeparam name="T">The type of elements stored in the queue.</typeparam>
 public class Queue<T>
 {
+    /// <summary>
+    /// Represents a single node in the queue's underlying linked list.
+    /// </summary>
     public class Node
     {
+        /// <summary>
+        /// The value stored in this node.
+        /// </summary>
         public T? value;
+
+        /// <summary>
+        /// A reference to the next node in the queue, or null if this is the last node.
+        /// </summary>
         public Node? next;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Node"/> class with the given value.
+        /// </summary>
+        /// <param name="value">The value to store in the node.</param>
         public Node(T? value)
         {
             this.value = value;
@@ -14,15 +32,34 @@ public class Queue<T>
         }
     }
 
+    /// <summary>
+    /// The first node in the queue.
+    /// </summary>
     public Node? head;
+
+    /// <summary>
+    /// The last node in the queue.
+    /// </summary>
     public Node? tail;
+
+    /// <summary>
+    /// The number of elements currently in the queue.
+    /// </summary>
     public int count;
 
+    /// <summary>
+    /// Returns the type of elements stored in the queue.
+    /// </summary>
+    /// <returns>The <see cref="Type"/> of <typeparamref name="T"/>.</returns>
     public Type CheckType()
     {
         return typeof(T);
     }
 
+    /// <summary>
+    /// Adds a value to the end of the queue.
+    /// </summary>
+    /// <param name="value">The value to add to the queue.</param>
     public void Enqueue(T? value)
     {
         Node newNode = new Node(value);
@@ -41,6 +78,10 @@ public class Queue<T>
         count++;
     }
 
+    /// <summary>
+    /// Returns the number of elements currently in the queue.
+    /// </summary>
+    /// <returns>The number of elements in the queue.</returns>
     public int Count()
     {
         return count;
