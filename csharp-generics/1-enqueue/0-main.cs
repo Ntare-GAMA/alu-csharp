4efd0b101717842038a87e1a2ff2156865a943a8
GAMA <a.ntare@alustudent.com>
2026-06-16 18:53:04 +0300

    Fix queue.cs and main, clean build no warnings

--- csharp-generics/1-enqueue/0-main.cs
+++ csharp-generics/1-enqueue/0-main.cs
@@ -4,24 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Queue<string> myStrQ = new Queue<string>();
+        Queue<int> myIntQ = new Queue<int>();
 
-        myStrQ.Enqueue("Hello");
-        Console.WriteLine("Number of nodes in queue: " + myStrQ.Count());
-
-        myStrQ.Enqueue("World");
-        Console.WriteLine("Number of nodes in queue: " + myStrQ.Count());
-
-        Console.WriteLine("----------");
-
-        Queue<float> myFloatQ = new Queue<float>();
-
-        myFloatQ.Enqueue(4.2f);
-        myFloatQ.Enqueue(4.2f);
-        myFloatQ.Enqueue(4.2f);
-        Console.WriteLine("Number of nodes in queue: " + myFloatQ.Count());
-
-        myFloatQ.Enqueue(-9.8f);
-        Console.WriteLine("Number of nodes in queue: " + myFloatQ.Count());
+        myIntQ.Enqueue(100);
+        myIntQ.Enqueue(101);
+        Console.WriteLine("Number of nodes in queue: " + myIntQ.Count());
     }
 }
